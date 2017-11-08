dbd1b3cb729868e8a26caf87af9b8a5ca9f4cf6f
HGohar <goharhovhannisyan19@gmail.com>
2017-11-08 17:38:00 +0400

    app

---
+++ app_server/routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var CtrlMain = require("../controllers/main")
+
+
+
+/* GET home page. */
+router.get('/', CtrlMain.index);
+
+module.exports = router;
